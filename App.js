1224f93c28abd429c12347470e77dc184c4b18d6
shanwije <shan.wijenayaka@ucdconnect.ie>
2017-10-17 23:05:26 +0530

    initial commit https://github.com/facebook/prop-types https://github.com/24ark/react-native-step-indicator

---
+++ App.js
@@ -0,0 +1,90 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import StepIndicator from 'react-native-step-indicator';
+
+const labels = ["Cart","Delivery Address","Order Summary","Payment Method","Track"];
+const customStyles = {
+  orientation:	'vertical',
+  stepIndicatorSize: 25,
+  currentStepIndicatorSize:30,
+  separatorStrokeWidth: 2,
+  currentStepStrokeWidth: 3,
+  stepStrokeCurrentColor: '#fe7013',
+  stepStrokeWidth: 3,
+  stepStrokeFinishedColor: '#fe7013',
+  stepStrokeUnFinishedColor: '#aaaaaa',
+  separatorFinishedColor: '#fe7013',
+  separatorUnFinishedColor: '#aaaaaa',
+  stepIndicatorFinishedColor: '#fe7013',
+  stepIndicatorUnFinishedColor: '#ffffff',
+  stepIndicatorCurrentColor: '#ffffff',
+  stepIndicatorLabelFontSize: 13,
+  currentStepIndicatorLabelFontSize: 13,
+  stepIndicatorLabelCurrentColor: '#fe7013',
+  stepIndicatorLabelFinishedColor: '#ffffff',
+  stepIndicatorLabelUnFinishedColor: '#aaaaaa',
+  labelColor: '#999999',
+  labelSize: 13,
+  currentStepLabelColor: '#fe7013'
+}
+
+export default class App extends Component<{}> {
+
+  constructor() {
+    super();
+    this.state = {
+        currentPosition: 2
+    }
+}
+  render() {
+    return (
+      <View style={{flex:1}}>
+          <StepIndicator
+         customStyles={customStyles}
+         currentPosition={this.state.currentPosition}
+         labels={labels}
+    />
+    </View>
+
+    );
+  }
+  onPageChange(position){
+    this.setState({currentPosition: position});
+}
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+
+
+
+
+
+
